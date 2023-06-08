83ceb6ecb755d08f5666c67463113df2e8988176
Loukas Theodosiou <theodosiou@evolbio.mpg.de>
2023-06-09 00:16:10 +0200

    :paw_prints: changing the function for r_gpseq_csub

--- R/replicate_gpseq_custom.R
+++ R/replicate_gpseq_custom.R
@@ -3,19 +3,50 @@
 #' @description
 #' #for each the parent sequence coming from the gpseq function, create n offspring sequences with a substitution rate of your interest.
 #'
-#' @param dna_seq A character, output from gpseq
-#' @param num_replicates A numeric value
-#' @param substitution_probs A list of probabilities for each based pair or an indel to appear
+#' @param dna_seq A character vector of DNA sequences.
+#' @param num_replicates An integer specifying the number of replicates for each DNA sequence.
+#' @param error_rate A number between 0 and 1 representing the error rate for base substitution.
+#' @param substitution_probs A list of length 5 containing the substitution probabilities for each base (A, C, G, T, and empty string).
 #'
-#' @return A data.frame
+#' @return A data frame containing the replicated barcode sequences.
 #'
 #' @examples
-#' dna_seq <- c("AAGA","AATC")
-#' substitution_probs <- list("A" = 0.1, "C" = 0.2, "G" = 0.3, "T" = 0.4, " " = 0.1)
-#' r_gpseq_csub(dna_seq,3,substitution_probs)
+#' # Example data
+#' dna_seq <- c("AAGA", "AATC")
+#' num_replicates <- 3
+#' error_rate <- 0.1
+#' substitution_probs <- list("A" = 0.1, "C" = 0.2, "G" = 0.3, "T" = 0.3, "" = 0.1)
+#'
+#' # Generate barcode sequences
+#' barcode_sequences <- r_generate_barcode(dna_seq, num_replicates, error_rate, substitution_probs)
+#'
+#' # Print the resulting barcode sequences
+#' print(barcode_sequences)
 #'
 #' @export
-r_gpseq_csub <- function(dna_seq, num_replicates, substitution_probs) {
+#'
+
+r_gpseq_csub <- r_generate_barcode <- function(dna_seq, num_replicates, error_rate, substitution_probs) {
+
+  # Validate input: dna_seq
+  if (!is.character(dna_seq) || any(!nzchar(dna_seq))) {
+    stop("Error: dna_seq should be a non-empty string or vector of DNA sequences.")
+  }
+
+  # Validate input: num_replicates
+  if (!is.integer(num_replicates) || num_replicates <= 0) {
+    if (is.numeric(num_replicates) && num_replicates %% 1 == 0 && num_replicates > 0) {
+      num_replicates <- as.integer(num_replicates)
+    } else {
+      stop("Error: num_replicates should be a positive integer.")
+    }
+  }
+
+  # Validate input: error_rate
+  if (!is.numeric(error_rate) || error_rate < 0 || error_rate > 1) {
+    stop("Error: error_rate should be a number between 0 and 1.")
+  }
+
   # Create a vector to store the replicated sequences
   replicated_dna <- vector(mode = "list", length = num_replicates * length(dna_seq))
 
@@ -33,12 +64,12 @@ r_gpseq_csub <- function(dna_seq, num_replicates, substitution_probs) {
         prob <- substitution_probs[[base]]
 
         # Generate a random number between 0 and 1
-        rand_num <- stats::runif(1)
+        rand_num <- runif(1)
 
-        # Check if the random number is less than the substitution probability
-        if (rand_num < prob) {
-          # If so, substitute the base with a random base or an empty string
-          new_base <- sample(c("A", "C", "G", "T", ""), size = 1, prob = c(rep(0.25, 4), 0.25))
+        # Check if an error occurs
+        if (rand_num < error_rate) {
+          # If an error occurs, select a random base for substitution
+          new_base <- sample(names(substitution_probs), size = 1, prob = unlist(substitution_probs))
         } else {
           # Otherwise, keep the original base
           new_base <- base
@@ -49,7 +80,7 @@ r_gpseq_csub <- function(dna_seq, num_replicates, substitution_probs) {
       }
 
       # Store the replicated sequence in the output vector
-      replicated_dna[[j + (i-1)*num_replicates]] <- list(parent = i, parent_seq = dna_seq[[i]], offspring = new_seq)
+      replicated_dna[[j + (i - 1) * num_replicates]] <- list(parent = i, parent_seq = dna_seq[[i]], offspring = new_seq)
     }
   }
 
@@ -59,3 +90,5 @@ r_gpseq_csub <- function(dna_seq, num_replicates, substitution_probs) {
   # Return the replicated sequences as a data frame
   return(replicated_df)
 }
+
+
